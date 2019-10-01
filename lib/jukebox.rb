9ae8d51b1f74236f733a1b68267d4e26b86530ec
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-01 22:34:56 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,4 +1,5 @@
-#def say_hello(name) Written for fun..
+# Written for fun..
+#def say_hello(name)
 #  "Hi #{name}!"
 #end
  
@@ -7,3 +8,62 @@
  
 #puts say_hello(users_name)
 
+def help()
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(my_songs)
+  my_songs.each_with_index do |song, index|
+    puts "#{index+1}. #{song}"
+  end
+end
+
+def play(my_songs)
+  puts "Please enter a song name or number:"
+  input = gets.chomp()
+  
+  if (1..9).to_a.index(input.to_i) != nil
+    puts "Playing #{my_songs[input.to_i - 1]}"
+  elsif my_songs.index(input) != nil
+    puts "Playing #{input}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox()
+  puts "Goodbye"
+end
+
+def prompt()
+  puts "Please enter a command:"
+  gets.chomp()
+end
+  
+def run(my_songs)
+  help()
+  input = prompt()
+  
+  while input != "exit"
+    if input == "list"
+      list(my_songs)
+      input = prompt()
+    elsif input == "play"
+      play(my_songs)
+      input = prompt()
+    elsif input == "help"
+      help()
+      input = prompt()
+    else
+      puts "Invalid command"
+      help()
+      input = prompt()
+    end
+  end
+  
+  exit_jukebox()
+end
