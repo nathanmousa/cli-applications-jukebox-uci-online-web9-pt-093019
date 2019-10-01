c987706b539b78deb1b2de6651b01bdaa5fd832d
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-01 22:35:07 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,13 +1,3 @@
-# Written for fun..
-#def say_hello(name)
-#  "Hi #{name}!"
-#end
- 
-#puts "Enter your name:"
-#users_name = gets.strip
- 
-#puts say_hello(users_name)
-
 def help()
   puts "I accept the following commands:"
   puts "- help : displays this help message"
